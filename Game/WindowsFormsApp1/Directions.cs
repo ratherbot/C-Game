d79e7e66de94fb366e074c63125d2be6063ff0e6
ratherbot <107281523+ratherbot@users.noreply.github.com>
2024-04-20 21:23:54 +0300

    Add files via upload
    Сырой прототип

---
+++ Game/WindowsFormsApp1/Directions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    enum Direction
+    {
+        Forward = 0,
+        Right = 1,
+        Backward = 2,
+        Left = 3,
+    }
+}
